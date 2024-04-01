483595d799b1b49dc75fca527fba2436a8ef1c6c
Ronit Sharma <sharmarp@vcu.edu>
2024-04-01 16:50:03 -0400

    Created my playlist page and added it to the App component

---
+++ my-app/src/PlaylistCreator.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './PlaylistCreator.css';
+
+const PlaylistCreator = () => {
+  const [playlistName, setPlaylistName] = useState('');
+  const [firstSong, setFirstSong] = useState('');
+
+  const handleCreatePlaylist = () => {
+    // Here, you would typically send the playlist data to a server or perform some other action
+    console.log(`Creating playlist "${playlistName}" with first song "${firstSong}"`);
+    // Reset the input fields after creating the playlist
+    setPlaylistName('');
+    setFirstSong('');
+  };
+
+  const handleCancel = () => {
+    // Reset the input fields
+    setPlaylistName('');
+    setFirstSong('');
+  };
+
+  return (
+    <div className="playlist-creator">
+      <h2>Create New Playlist</h2>
+      <div className="input-group">
+        <label htmlFor="playlistName">Playlist Name:</label>
+        <input
+          type="text"
+          id="playlistName"
+          value={playlistName}
+          onChange={(e) => setPlaylistName(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="firstSong">First Song:</label>
+        <input
+          type="text"
+          id="firstSong"
+          value={firstSong}
+          onChange={(e) => setFirstSong(e.target.value)}
+        />
+      </div>
+      <div className="button-group">
+        <button onClick={handleCreatePlaylist}>Create Playlist</button>
+        <button onClick={handleCancel}>Cancel</button>
+      </div>
+    </div>
+  );
+};
+
+export default PlaylistCreator;
