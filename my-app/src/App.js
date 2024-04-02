277eef8bafcd95d9af8ef058f3b23f6e7713f208
Ronit Sharma <sharmarp@vcu.edu>
2024-04-02 11:40:47 -0400

    Rendered the Login Page to the app.js file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import PlaylistCreator from './PlaylistCreator';
 import './PlaylistCreator.css';
-import login from './login';
-
+import Login from './login'; // Import the Login component
 
 function App() {
   return (
     <div className="App">
-      
       <main>
-        <PlaylistCreator />     <login />
+        <PlaylistCreator />
+        <Login /> {/* Render the Login component */}
       </main>
-      
     </div>
   );
 }
